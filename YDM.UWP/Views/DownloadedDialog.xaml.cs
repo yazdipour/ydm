33acdb2d1f344bc57036af0161bfcff384634b29
Shahriar Yazdipour <shahriar.yazdipour@outlook.com>
2018-12-30 23:20:02 +0330

    Bring Old Uwp.Views

---
+++ YDM.UWP/Views/DownloadedDialog.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.Xaml.Controls;
+using YDM.Share.Models;
+using System.Reactive.Linq;
+using Akavache;
+using YDM.Share;
+
+namespace YDM.UWP.Views
+{
+	public sealed partial class DownloadedDialog : ContentDialog
+	{
+		#region Head
+		public string changed = "";
+		internal DownloadedItems dl;
+
+		public DownloadedDialog()
+		{
+			InitializeComponent();
+		}
+		private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			Hide();
+		}
+		#endregion
+
+		private async void ContentDialog_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			try
+			{
+				oTitle.Text = dl.Title;
+				Duration.Text = "Duration : " + dl.Duration;
+				View.Text = dl.Views + " Views";
+				Img.ImageSource = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri(dl.Img, UriKind.Absolute));
+				await LoadListAsync();
+				DataTransferManager dataTransferManager = DataTransferManager.GetForCurrentView();
+				dataTransferManager.DataRequested += DataTransferManager_DataRequested;
+			}
+			catch
+			{
+				Hide();
+			}
+		}
+
+		private async System.Threading.Tasks.Task LoadListAsync()
+		{
+			try
+			{
+				xlist.ItemsSource = await BlobCache.LocalMachine.GetObject<LinkItems[]>("LI" + dl.Id);
+			}
+			catch { }
+		}
+
+		private async void Link_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			string url = txtBox.Text;
+			switch ((sender as Button).Tag.ToString())
+			{
+				case "0":
+					//web
+					await Windows.System.Launcher.LaunchUriAsync(new Uri(url));
+					break;
+				case "1":
+					//copy
+					var dataPackage = new DataPackage();
+					dataPackage.SetText(url);
+					Clipboard.SetContent(dataPackage);
+					break;
+				case "2":
+					//stream
+					changed = url;
+					Hide();
+					break;
+				case "3":
+					//share
+					DataTransferManager.ShowShareUI();
+					break;
+			}
+		}
+
+		private void DataTransferManager_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+		{
+			DataRequest request = args.Request;
+			request.Data.SetText(txtBox.Text + "\n\n Watch Youtube Videos without censorship with YDM: https://goo.gl/QM2KC5");
+			request.Data.Properties.Title = dl.Title;
+		}
+
+		private async void xlist_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			FindName("txtBox");
+			FindName("btnBox");
+			var clicked = xlist.SelectedItem as LinkItems;
+			//txtBox.Text = Api.GetVideoLink(dl.Id, clicked.tag);
+			//sizeTxt.Text = "";
+			//sizeLoading.IsIndeterminate = true;
+			//string size = await Api.FillSizeAsync(dl.Id, clicked.url, xlist.SelectedIndex);
+			//sizeTxt.Text = "File size: " + size;
+			sizeLoading.IsIndeterminate = false;
+		}
+
+		private async void OpenYT_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			Uri uri = new Uri("http://www.youtube.com/watch?v=" + dl.Id);
+			await Windows.System.Launcher.LaunchUriAsync(uri);
+		}
+
+		private async void rm_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			//ReadLocalValue it 1st
+			try
+			{
+				var ser = await BlobCache.LocalMachine.GetObject<List<DownloadedItems>>("MainList");
+				ser.Remove(ser.Find(obj => obj.Id == dl.Id));
+				//save changes
+				await BlobCache.LocalMachine.InsertObject("MainList", ser);
+				await BlobCache.LocalMachine.Invalidate("LI" + dl.Id);
+				changed = "reload";
+			}
+			catch { }
+			Hide();
+		}
+	}
+}
---
+++ YDM.UWP/Views/SearchPanel.xaml.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using YDM.Share.Models;
+
+namespace YDM.UWP.Views
+{
+	public sealed partial class SearchPanel : Page
+	{
+		public string Agent { get; set; } = null;
+
+		public SearchPanel()
+		{
+			this.InitializeComponent();
+		}
+
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			base.OnNavigatedTo(e);
+			Agent = e.Parameter as string;
+		}
+
+		private void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			//Microsoft.AppCenter.Analytics.Analytics.TrackEvent("Search Usage");
+			switch (Agent)
+			{
+				case "search":
+					FindName("SearchGrid"); 
+					FindName("cmBox"); 
+					break;
+				case "playlist":
+					Title.Text = "Get Playlist";
+					FindName("SearchGrid");
+					searchField.PlaceholderText = "http://www.youtube.com/playlist?list=PlayListID";
+					searchButton.Content = "Explore";
+					break;
+				case "advance":
+					Title.Text = "Advance";
+					FindName("AdvGrid");
+					break;
+			}
+		}
+
+		private async void SlidableListItem_RightCommandRequested(object sender, EventArgs e)
+		{
+			//try
+			//{
+			//	var slidableitem = sender as SlidableListItem;
+			//	var item = slidableitem.DataContext as DownloadedItems;
+			//	var key = await Api.GetVideo(item.Id);
+			//	if (key == null) return;
+			//	//await Api.FillSizeAsync(key);
+			//}
+			//catch { Utils.ShowMSG("Error!"); }
+		}
+		private async void SlidableListItem_LeftCommandRequested(object sender, EventArgs e)
+		{
+			//try
+			//{
+			//	var slidableitem = sender as SlidableListItem;
+			//	var item = slidableitem.DataContext as DownloadedItems;
+			//	Uri uri = new Uri("http://www.youtube.com/watch?v=" + item.Id);
+			//	await Windows.System.Launcher.LaunchUriAsync(uri);
+			//}
+			//catch { }
+		}
+
+		private async void lst1_ItemClick(object sender, ItemClickEventArgs e)
+		{
+			//try
+			//{
+			//	var item = e.ClickedItem as DownloadedItems;
+			//	var key = await Api.GetVideo(item.Id);
+			//	if (key == null) return;
+			//	//await Api.FillSizeAsync(key);
+			//}
+			//catch { Utils.ShowMSG("Error!"); }
+		}
+		/// <summary>
+		/// Pivot 1
+		/// </summary>
+		private async void LoadItemsAsync()
+		{
+			//MotherPanel.StaticRing.IsLoading = true;
+			//if (Agent == "search")
+			//{
+			//	var str = searchField.Text.Trim();
+			//	string maxRes = (cmBox.SelectedItem as ComboBoxItem).Content.ToString();
+			//	if (str.Length < 2) { MotherPanel.StaticRing.IsLoading = false; return; }
+			//	var dlr = await Api.SearchVideo(str, maxRes);
+			//	if (dlr == null)
+			//	{
+			//		Utils.ShowMSG("Err");
+			//		MotherPanel.StaticRing.IsLoading = false;
+			//		return;
+			//	}
+			//	lst.ItemsSource = dlr;
+			//}
+			//else
+			//{
+			//	var str = searchField.Text.Trim();
+			//	if (str.Length < 2) { MotherPanel.StaticRing.IsLoading = false; return; }
+			//	var dlr = await Api.GetPlayList(str);
+			//	if (dlr == null)
+			//	{
+			//		Utils.ShowMSG("Err");
+			//		MotherPanel.StaticRing.IsLoading = false;
+			//		return;
+			//	}
+			//	lst.ItemsSource = dlr;
+			//}
+			//MotherPanel.StaticRing.IsLoading = false;
+		}
+		private void searchField_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+		{
+			if (e.Key == Windows.System.VirtualKey.Enter)
+			{
+				LoadItemsAsync();
+				// Make sure to set the Handled to true, otherwise the RoutedEvent might fire twice
+				e.Handled = true;
+			}
+		}
+		/// <summary>
+		/// Pivot 2 PL
+		/// </summary>
+		private void CpButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			//try
+			//{
+			//	List<DownloadedItems> x = (lst.ItemsSource as List<DownloadedItems>);
+			//	if (x.Count > App.Usr.nrCanDownload)
+			//	{
+			//		Utils.ShowMSG("You can't download " + x.Count + " videos with your account");
+			//	}
+			//	else
+			//	{
+			//		List<string> ls = new List<string>();
+			//		foreach (var item in x)
+			//			ls.Add(item.Id);
+			//		Api.GetBatchOfVideos(ls);
+			//	}
+			//}
+			//catch { }
+		}
+		private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            //	try
+            //	{
+            //		if (Agent=="playlist") FindName("GetAll");
+            //		LoadItemsAsync();
+            //	}
+            //	catch
+            //	{
+            //		Utils.ShowMSG("Something happend!");
+            //	}
+        }
+    /// <summary>
+    /// Pivot 3 WL
+    /// </summary>
+    List<CheckBox> ListCheck = null;
+
+		private void Button_Click_1(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			//string html = htmlTextBox.Text.Trim();
+			//if (html.Length < 20) return;
+			//ListCheck = new List<CheckBox>();
+			//var count = App.Usr.nrCanDownload;
+			//var tempList = new List<string>();
+			//if (htmlRadio.IsChecked == true)
+			//{
+			//	string filter = FilterBox.Text.Trim();
+			//	string ele = EleBox.Text.Trim();
+			//	if (filter.Length < 2) return;
+			//	Regex regex = new Regex(@"<"+ ele +"(.*?)" + filter + @"=""(.*?)""");
+			//	var matchs = regex.Matches(html);
+			//	foreach (Match match in matchs)
+			//	{
+			//		try
+			//		{
+			//			var m = match.Value.Substring(filter.Length + 1).Replace("\"", "");
+			//			if (m.Length < 6) continue;
+			//			if (m.Contains("?v=")) { m = m.Substring(m.IndexOf("?v=") + 3); }
+			//			tempList.Add(m);
+			//		}
+			//		catch { }
+			//	}
+			//	tempList = tempList.Distinct().ToList();
+			//	foreach (var item in tempList)
+			//		ListCheck.Add(new CheckBox() { Content = "https://www.youtube.com/watch?v=" + item.Trim(), IsChecked = (--count > 0) });
+			//}
+			//else
+			//{
+			//	tempList = new List<string>(html.Split(new[] { "\r\n", "\r", "\n" },StringSplitOptions.None));
+			//	foreach (var item in tempList)
+			//		ListCheck.Add(new CheckBox() { Content = item.Trim() });
+			//}
+			
+			//LstParent.Visibility = Windows.UI.Xaml.Visibility.Visible;
+			//FindName("Selecters");
+			//LstChk.ItemsSource = ListCheck;
+		}
+
+		private void Button_Click_2(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            //	var ls = ListCheck.FindAll(obj => obj.IsChecked == true);
+            //	if (ls.Count > App.Usr.nrCanDownload)
+            //	{
+            //		Utils.ShowMSG("You can't download " + ls.Count + " videos with your account");
+            //	}
+            //	else
+            //	{
+            //		var lss = new List<string>();
+            //		foreach (var item in ls)
+            //		{
+            //			lss.Add(item.Content.ToString());
+            //		}
+            //		try
+            //		{
+            //			Api.GetBatchOfVideos(lss);
+            //		}
+            //		catch { }
+            //	}
+        }
+
+    private void Button_Click_De(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			foreach (var item in (ListCheck.FindAll(obj => obj.IsChecked == true)))
+				item.IsChecked = false;
+			LstChk.ItemsSource = ListCheck;
+		}
+
+		private void Button_Click_Se(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			foreach (var item in (ListCheck.FindAll(obj => obj.IsChecked == false)))
+				item.IsChecked = true;
+			LstChk.ItemsSource = ListCheck;
+		}
+	}
+}
