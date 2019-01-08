057bbdfb372608a4099a835f3959af04df258f14
Shahriar Yazdipour <shahriar.yazdipour@outlook.com>
2019-01-08 23:46:02 +0330

    Minor changes + Put VideoView on its file

---
+++ YDM.WebClient/src/components/VideoView.vue
@@ -0,0 +1,30 @@
+<template>
+ <v-layout ma-1 xs12 md7 align-center justify-center>
+                  <v-card class="white--text">
+                    <v-layout>
+                      <v-flex>
+                        <v-img
+                          src="https://ydm.herokuapp.com/getimage.php?i=tsMVj1VxSOU"
+                          height="125px"
+                        ></v-img>
+                      </v-flex>
+                      <v-flex xs7>
+                        <v-card-title primary-title v-contain>
+                          <div>
+                            <div class="headline">Supermodel</div>
+                            <div>Foster the People</div>
+                            <a href="https://ydm.herokuapp.com/getimage.php?i=tsMVj1VxSOU">tsMVj1VxSOU</a>
+                          </div>
+                        </v-card-title>
+                      </v-flex>
+                    </v-layout>
+                    <v-divider light></v-divider>
+                    <v-card-actions>
+                      <span class="body-2 ma-2">hd720</span> <a href="https://ydm.herokuapp.com/getimage.php?i=tsMVj1VxSOU">https://ydm.herokuapp.com/getimage.php?i=tsMVj1VxSOU%herokuapp</a>
+                    </v-card-actions>
+                    <v-card-actions>
+                      <span class="body-2 ma-2">hd720</span> <a href="https://ydm.herokuapp.com/getimage.php?i=tsMVj1VxSOU">https://ydm.herokuapp.com/getimage.php?i=tsMVj1VxSOU%herokuapp</a>
+                    </v-card-actions>
+                  </v-card>
+              </v-layout>
+</template>
