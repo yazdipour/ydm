2f6c96bb0673e460edbae8ca0b34ff3a72a045f4
Shahriar Yazdipour <shahriar.yazdipour@outlook.com>
2019-01-08 23:29:25 +0330

    Video Item basic UI

--- YDM.WebClient/src/App.vue
+++ YDM.WebClient/src/App.vue
@@ -6,45 +6,66 @@
         extended
         flat>
         <v-card-text>
-          <h1 class="text-md-center display-1 font-weight-light">YDM</h1>
+          <h1 class="text-xs-center display-1 font-weight-light">YDM</h1>
         </v-card-text>
       </v-toolbar>
   
       <v-layout row pb-2 style="background: #303030;">
-        <v-flex xs8 offset-xs2>
+        <v-flex xs10 offset-xs1 md6 offset-md3>
           <v-card class="card--flex-toolbar" style="margin-top: -64px;background: #212121;">
             <v-toolbar card prominent>
               <v-text-field color="white" label="Youtube Video URL / Search / PlayList" style="margin-top: 12px"></v-text-field>
-              <v-spacer></v-spacer>
               <v-btn round color="red" dark><v-icon>get_apps</v-icon>GET</v-btn>
-              <v-btn round color="#444"><v-icon>search</v-icon>Search</v-btn>
+              <v-btn icon color="error"><v-icon>search</v-icon></v-btn>
             </v-toolbar>
             <v-divider></v-divider>
-            <v-card-text style="height: 200px;">
-            
-              <v-btn icon><v-icon>invert_colors</v-icon></v-btn>
-              <v-btn icon><v-icon>flip_to_front</v-icon></v-btn>
-              <v-btn icon><v-icon>important_devices</v-icon></v-btn>
-              <v-btn icon><v-icon>launch</v-icon></v-btn>
-              <v-btn icon><v-icon>play_circle_filled</v-icon></v-btn>
-              <v-btn icon><v-icon>link</v-icon></v-btn>
-            
+            <v-card-text style="min-height: 200px;">
+              <v-layout ma-1 xs12 md7 align-center justify-center>
+                  <v-card class="white--text">
+                    <v-layout>
+                      <v-flex>
+                        <v-img
+                          src="https://ydm.herokuapp.com/getimage.php?i=tsMVj1VxSOU"
+                          height="125px" contain
+                        ></v-img>
+                      </v-flex>
+                      <v-flex xs7>
+                        <v-card-title primary-title v-contain>
+                          <div>
+                            <div class="headline">Supermodel</div>
+                            <div>Foster the People</div>
+                            <a href="https://ydm.herokuapp.com/getimage.php?i=tsMVj1VxSOU">tsMVj1VxSOU</a>
+                          </div>
+                        </v-card-title>
+                      </v-flex>
+                    </v-layout>
+                    <v-divider light></v-divider>
+                    <v-card-actions>
+                      <span class="body-2 ma-2">hd720</span> <a href="https://ydm.herokuapp.com/getimage.php?i=tsMVj1VxSOU">https://ydm.herokuapp.com/getimage.php?i=tsMVj1VxSOU%herokuapp</a>
+                    </v-card-actions>
+                    <v-card-actions>
+                      <span class="body-2 ma-2">hd720</span> <a href="https://ydm.herokuapp.com/getimage.php?i=tsMVj1VxSOU">https://ydm.herokuapp.com/getimage.php?i=tsMVj1VxSOU%herokuapp</a>
+                    </v-card-actions>
+                  </v-card>
+              </v-layout>
             </v-card-text>
-
             <!-- link to App and Github Pages-->
           </v-card>
         </v-flex>
       </v-layout>
     </v-card>
+  <footer></footer>
   </v-app>
 </template>
-
 <script>
+import Footer from './components/Footer.vue'
   export default {
     data: () => ({
       drawer: null
     }),
-
+    components: {
+      'footer': Footer,
+    },
     props: {
       source: String
     }
---
+++ YDM.WebClient/src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-footer
+    dark
+    height="auto">
+    <v-card
+      class="flex"
+      flat
+      tile>
+      <v-card-title class="teal">
+        <strong class="subheading">Get connected with us on social networks!</strong>
+    <a href="https://github.com/yazdipour/ydm">Github/yazdipour/YDM</a>
+        <v-spacer></v-spacer>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-3"
+          dark
+          icon>
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-actions class="grey darken-3 justify-center">
+            <div>&copy; {{ new Date().getFullYear() }}<strong>YDM</strong></div>
+      </v-card-actions>
+    </v-card>
+  </v-footer>
+</template>
